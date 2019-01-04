00fbb033895ce37eeca8443d4d54980a0c1187d2
Joe Mullen <mullenjoe13@gmail.com>
2019-01-04 04:29:16 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  if (array.size == 1)
+    array.join(",")
+  elsif (array.size == 2)
+    array.join(" and ")
+ elsif (array.size >= 3)
+    end_of_array = array.pop
+    array_string = array.join(", ")
+    array_string +", and " + end_of_array
+  end
 end
